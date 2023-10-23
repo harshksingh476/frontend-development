2c4686365db00c8f0ee9c0e7e77e09a54c72698b
Harisha-aavana <62471934+Harisha-aavana@users.noreply.github.com>
2023-10-23 16:52:24 +0530

    for web design

--- repos/config.py
+++
@@ -1,6 +0,0 @@
-
-class Config:
-    SECRET_KEY = 'kdsfjsddfskmsdkmdkmsfmksdlfmds'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://doadmin:AVNS_Sdg73H69oVDnmu7zGJy@db-mysql-respo-do-user-14714950-0.b.db.ondigitalocean.com:25060/production_cms_repos'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = 'repos/static/imageupload/'
